68054df0c317f2e7bf53a876a7cff57f954e1058
Mike Donnici <michael@mesa.net.au>
2018-06-29 09:48:09 +1000

    bubblesort kata

--- practice/kata/go/bubblesort/bubblesort.go
+++ practice/kata/go/bubblesort/bubblesort.go
@@ -1,18 +1,14 @@
 package bubblesort
 
-// BubbleSort runs through a slice of numbers once for each element in the slice
-// and pushed the largest number to its correct position on each pass.
 func BubbleSort(numbers []int) []int {
 
-	l := len(numbers)
-
-	for i := 0; i < l; i++ {
-		for j := 0; j < (l-1-i); j++ {
-			if numbers[j] > numbers[j+1] {
-				numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+	for i := 0; i < len(numbers); i++ {
+	 	for j := 0; j < len(numbers) - 1 - i; j++ {
+	 		if numbers[j] > numbers[j+1] {
+	 			numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
 			}
 		}
-	}
+	 }
 
-	return numbers
+	 return numbers
 }
--- practice/kata/go/bubblesort/bubblesort_test.go
+++ practice/kata/go/bubblesort/bubblesort_test.go
@@ -2,34 +2,33 @@ package bubblesort
 
 import (
 	"testing"
-	"fmt"
 	"reflect"
+	"fmt"
 )
 
 func TestBubbleSort(t *testing.T) {
-
-	cases := []struct {
-		input  []int
+	cases := []struct{
+		input []int
 		expect []int
 	}{
 		{
-			[]int{1, 2, 3, 4, 5},
-			[]int{1, 2, 3, 4, 5},
+			input: []int{2,1},
+			expect: []int{1,2},
 		},
 		{
-			[]int{5, 4, 3, 2, 1},
-			[]int{1, 2, 3, 4, 5},
+			input: []int{9,8,7,6,5,4,3,2,1},
+			expect: []int{1,2,3,4,5,6,7,8,9},
 		},
 		{
-			[]int{3, 4, 2, 5, 1},
-			[]int{1, 2, 3, 4, 5},
+			input: []int{1,1,1,1,3,2,1,1,1,1},
+			expect: []int{1,1,1,1,1,1,1,1,2,3},
 		},
 	}
 
 	for _, c := range cases {
-		result := BubbleSort(c.input)
-		if !reflect.DeepEqual(result, c.expect) {
-			t.Fatalf(fmt.Sprintf("For input %v expected %v, got %v", c.input, c.expect, result))
+		sorted := BubbleSort(c.input)
+		if !reflect.DeepEqual(sorted, c.expect) {
+			t.Fatalf(fmt.Sprintf("For input %v, expected %v, got %v", c.input, c.expect, sorted))
 		}
 	}
 }
