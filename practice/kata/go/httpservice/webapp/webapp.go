9622dc43e929f1b4945de19fed074914af0e1094
Mike Donnici <michael@mesa.net.au>
2018-06-28 14:20:25 +1000

    httpservice kata

--- practice/kata/go/httpservice/webapp/webapp.go
+++ practice/kata/go/httpservice/webapp/webapp.go
@@ -7,70 +7,67 @@ import (
 )
 
 type server struct {
-	port string
 	router *mux.Router
 }
 
-// New returns a pointer to a server
-func New(port string) *server {
+func New() *server {
 	s := &server{
-		port: port,
 		router: mux.NewRouter(),
 	}
 	s.routes()
 	return s
 }
 
-func (s *server)routes() {
-	s.router.Handle("/", s.handleIndex())
-	s.router.Handle("/hello", s.handleHello())
-	s.router.Handle("/hello/{name}", s.handleHelloName())
-	s.router.Handle("/hello.json", s.handleHelloJSON())
-	s.router.Handle("/admin/{token}", s.admin(s.handleAdmin()))
-}
-
-// ServeHTTP ...
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	s.router.ServeHTTP(w, r)
 }
 
+func (s *server) routes() {
+	s.router.Handle("/", s.handleIndex())
+	s.router.Handle("/hello", s.handleHello())
+	s.router.Handle("/hello.json", s.handleHelloJSON())
+	s.router.Handle("/admin/{token}", s.admin(s.handleAdmin()))
+}
+
 func (s *server) handleIndex() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, "/hello", http.StatusFound)
+		w.Header().Set("Location", "/hello")
+		w.WriteHeader(301)
 	}
 }
 
 func (s *server) handleHello() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w,"hello")
-	}
-}
-
-func (s *server) handleHelloName() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "hello " + mux.Vars(r)["name"])
+		io.WriteString(w, "hello")
 	}
 }
 
 func (s *server) handleHelloJSON() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		io.WriteString(w,`{"response": "hello"}`)
-	}
-}
-
-func (s *server) handleAdmin() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w,"admin authorized")
+		io.WriteString(w, `{"response": "hello"}`)
 	}
 }
 
 func (s *server) admin(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		token := mux.Vars(r)["token"]
-		if token == "abc123" {
-			h(w,r)
+		v := mux.Vars(r)
+		if v["token"] == "abc123" {
+			h(w, r)
 		}
-		w.WriteHeader(http.StatusUnauthorized)
+		unauthorized(w, r)
 	}
 }
+
+func (s *server) handleAdmin() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "admin authorized")
+	}
+}
+
+func unauthorized(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusUnauthorized)
+}
+
+
+
--- practice/kata/go/httpservice/webapp/webapp_test.go
+++ practice/kata/go/httpservice/webapp/webapp_test.go
@@ -3,80 +3,69 @@ package webapp
 import (
 	"testing"
 	"github.com/matryer/is"
-	"net/http"
 	"net/http/httptest"
 	"strings"
 )
 
-// TestNew tests the creation of a server value
 func TestNew(t *testing.T) {
-	is := is.New(t)
-	srv := New("8080")
-	is.Equal(srv.port, "8080")
+	srv := New()
+	t.Log(srv)
 }
 
-// TestIndexHandler tests the index handler which should return a 302 status and redirect
-func TestIndexHandler(t *testing.T) {
+func TestNotFound(t *testing.T) {
 	is := is.New(t)
-	srv := New("8080")
-	r, err := http.NewRequest("GET", "/", nil)
-	is.NoErr(err) // request error
+	srv := New()
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/notfound", nil)
+	srv.ServeHTTP(w, r)
+	is.Equal(w.Code, 404) // should get 404 Not Found
+}
+
+func TestHello(t *testing.T) {
+	is := is.New(t)
+	srv := New()
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/hello", nil)
+	srv.ServeHTTP(w, r)
+	is.Equal(w.Code, 200)
+}
+
+func TestIndex(t *testing.T) {
+	is := is.New(t)
+	srv := New()
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
+	srv.ServeHTTP(w, r)
+	is.Equal(w.Code, 301)
+	is.Equal(w.Result().Header.Get("Location"), "/hello")
+}
+
+func TestHelloJSON(t *testing.T) {
+	is := is.New(t)
+	srv := New()
+	r := httptest.NewRequest("GET", "/hello.json", nil)
 	w := httptest.NewRecorder()
 	srv.ServeHTTP(w, r)
-	is.Equal(w.Code, 302) // status code should be 302
-}
-
-func TestHelloHandler(t *testing.T) {
-	is := is.New(t)
-	srv := New("8080")
-	r, err := http.NewRequest("GET", "/hello", nil)
-	is.NoErr(err) // request error
-	w := httptest.NewRecorder()
-	srv.ServeHTTP(w, r)
-	is.Equal(w.Code, 200) // status not 200 OK
-	body := w.Body.String()
-	is.Equal(body, "hello") // body should be "hello"
-}
-
-func TestHelloNameHandler(t *testing.T) {
-	is := is.New(t)
-	srv := New("")
-	r, err := http.NewRequest("GET", "/hello/mike", nil)
-	is.NoErr(err) // request error
-	w := httptest.NewRecorder()
-	srv.ServeHTTP(w,r)
-	is.Equal(w.Code, 200) // status not 200 OK
-	body := w.Body.String()
-	is.Equal(body, "hello mike")
-}
-
-func TestHelloJSONHandler(t *testing.T) {
-	is := is.New(t)
-	srv := New("")
-	w := httptest.NewRecorder()
-	r, err := http.NewRequest("GET", "/hello.json", nil)
-	is.NoErr(err) // request error
-	srv.ServeHTTP(w, r)
-	is.Equal(w.Code, 200) // status should be 200 OK
-	is.Equal(w.Header().Get("Content-Type"), "application/json") // wrong content type header
+	is.Equal(w.HeaderMap.Get("Content-Type"), "application/json")
 	body := strings.Replace(w.Body.String(), " ", "", -1)
 	is.Equal(body, `{"response":"hello"}`)
 }
 
 func TestAdminMiddleware(t *testing.T) {
 	is := is.New(t)
-	srv := New("")
+	srv := New()
 
+	r := httptest.NewRequest("GET", "/admin/abc123", nil)
 	w := httptest.NewRecorder()
-	r, err := http.NewRequest("GET", "/admin/abc123", nil)
-	is.NoErr(err) // request error
 	srv.ServeHTTP(w, r)
 	is.Equal(w.Code, 200)
+	body := w.Body.String()
+	is.Equal(body, "admin authorized")
 
+	r = httptest.NewRequest("GET", "/admin/abc124", nil)
 	w = httptest.NewRecorder()
-	r, err = http.NewRequest("GET", "/admin/abc124", nil)
-	is.NoErr(err) // request error
 	srv.ServeHTTP(w, r)
-	is.Equal(w.Code, 401) // expected 401 unauthorized
+	is.Equal(w.Code, 401)
 }
+
 
