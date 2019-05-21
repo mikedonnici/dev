c0b1cb9b69be0b7af2b7fb66e145eb2eade42fde
Mike Donnici <michael@mesa.net.au>
2019-05-21 16:10:47 +1000

    Update bubblesort.go

--- practice/kata/go/bubblesort/bubblesort.go
+++ practice/kata/go/bubblesort/bubblesort.go
@@ -3,6 +3,7 @@ package bubblesort
 // BubbleSort sorts a slice of int.
 // Note this version is LESS efficient than the previous as it
 // iterates through the entire slice for each comparison
+// more efficient version: https://play.golang.org/p/_pmWoHabfgq
 func BubbleSort(numbers []int) []int {
 	for i := 0; i < len(numbers); i++ {
 		for j := 0; j < len(numbers); j++ {
