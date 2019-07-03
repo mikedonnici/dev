ba1c3561f7cd47b4de21292f551b66483710d675
Mike Donnici <michael@mesa.net.au>
2019-07-03 11:21:50 +1000

    tidy dining philosophers

--- go/concurrency/diningphil/main.go
+++ go/concurrency/diningphil/main.go
@@ -11,9 +11,7 @@ const maxEats = 3
 type chopstick sync.Mutex
 
 type philosopher struct {
-	Index          int
-	LeftChopstick  sync.Mutex
-	RightChopstick sync.Mutex
+	Index int
 }
 
 var philosophers []philosopher
@@ -21,7 +19,6 @@ var chopsticks []sync.Mutex
 var wg sync.WaitGroup
 
 func init() {
-
 	for i := 0; i < 5; i++ {
 		philosophers = append(philosophers, philosopher{Index: i})
 		chopsticks = append(chopsticks, sync.Mutex{})
@@ -55,7 +52,7 @@ func (p *philosopher) eat() {
 	msg := "Starting to eat %d (with chopsticks %d and %d)\n"
 	fmt.Printf(msg, p.Index+1, left+1, right+1)
 	time.Sleep(1 * time.Second)
-	fmt.Printf("Finishing eating %d\n", p.Index + 1)
+	fmt.Printf("Finishing eating %d\n", p.Index+1)
 	// release the chopsticks
 	chopsticks[left].Unlock()
 	chopsticks[right].Unlock()
@@ -66,11 +63,7 @@ func (p *philosopher) eat() {
 // by getting a lock on the required chopsticks. It communicates
 // the permission over a channel.
 func hostPermission(leftChopstick, rightChopstick *sync.Mutex, permission chan int) {
-	// these will block until gained
 	leftChopstick.Lock()
 	rightChopstick.Lock()
 	permission <- 1
-	// <- finished
-	// leftChopstick.Unlock()
-	// rightChopstick.Unlock()
 }
