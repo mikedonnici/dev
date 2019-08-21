40db7fe4e59caa9dbdaa9879fc3fa9f8419925e5
Mike Donnici <michael@mesa.net.au>
2019-08-21 11:37:49 +1000

    kotlin

--- practice/exercism/kotlin/hamming/src/main/kotlin/Hamming.kt
+++ practice/exercism/kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -1,15 +1,8 @@
 object Hamming {
     fun compute(dna1: String, dna2: String): Int {
-        var hdist = 0
-
-        if (dna1.length != dna2.length) {
+        if (dna1.length != dna2.length)
             throw IllegalArgumentException("left and right strands must be of equal length.")
-        }
 
-        for (i in dna1.indices) {
-            if (dna1[i] != dna2[i]) hdist += 1
-        }
-
-        return hdist
+        return dna1.zip(dna2).count { it.first != it.second }
     }
 }
