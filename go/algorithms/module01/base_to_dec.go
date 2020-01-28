5076c02d42c89784dc0b249f55cbad448bf3ad96
Mike Donnici <michael@mesa.net.au>
2020-01-28 20:53:21 +1100

    go algorithms - base_to_dec

--- go/algorithms/module01/base_to_dec.go
+++ go/algorithms/module01/base_to_dec.go
@@ -1,5 +1,7 @@
 package module01
 
+import "math"
+
 // BaseToDec takes in a number and the base it is currently
 // in and returns the decimal equivalent as an integer.
 //
@@ -9,5 +11,15 @@ package module01
 //   BaseToDec("1110", 2) => 14
 //
 func BaseToDec(value string, base int) int {
-	return 0
+	charToDecimal := map[string]float64 {
+		"1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8,
+		"9": 9, "A": 10, "B": 11, "C": 12, "D": 13, "E": 14, "F": 15,
+	}
+	var result float64
+	maxIndex := len(value)-1
+	for i := maxIndex; i >= 0; i-- {
+		pos := maxIndex - i
+		result += charToDecimal[string(value[i])] * (math.Pow(float64(base), float64(pos)))
+	}
+	return int(result)
 }
