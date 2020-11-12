e94b9affb8e43232d4371ccb57c05050efb2a162
Mike Donnici <michael@mesa.net.au>
2020-11-13 10:33:57 +1100

    go algorithms

--- go/algorithms/module01/dec_to_base.go
+++ go/algorithms/module01/dec_to_base.go
@@ -1,5 +1,7 @@
 package module01
 
+import "fmt"
+
 // DecToBase will return a string representing
 // the provided decimal number in the provided base.
 // This is limited to bases 2-16 for simplicity.
@@ -9,6 +11,26 @@ package module01
 //   DecToBase(14, 16) => "E"
 //   DecToBase(14, 2) => "1110"
 //
+
 func DecToBase(dec, base int) string {
-	return ""
+
+	var result string
+
+	// divide the number by the base - the remainder is the number that belongs in the current position
+	result = numString(dec % base) + result
+
+
+	// quotient less than 1 we are done - no fractions here
+	quotient := dec / base
+	if quotient < 1 {
+		return result
+	}
+
+	// recursive call to prepend the next number to the final result
+	return DecToBase(quotient, base) + result
+}
+
+// numString returns the string representation of a single digit number up to base 16
+func numString(n int) string {
+	return fmt.Sprintf("%X", n)
 }
--- go/algorithms/module01/fizz_buzz.go
+++ go/algorithms/module01/fizz_buzz.go
@@ -1,5 +1,11 @@
 package module01
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 // FizzBuzz will print out all of the numbers
 // from 1 to N replacing any divisible by 3
 // with "Fizz", and divisible by 5 with "Buzz",
@@ -11,4 +17,21 @@ package module01
 // I wouldn't normally recommend this, but did
 // it here to make life easier for beginners.
 func FizzBuzz(n int) {
+	var xs []string
+	for i := 1; i <= n; i++ {
+		xs = append(xs, fizzBuzzOrNumber(i))
+	}
+	fmt.Printf("%s\n", strings.Join(xs, ", "))
+}
+
+func fizzBuzzOrNumber(n int) string {
+	switch {
+	case n%5 == 0 && n%3 == 0:
+		return "Fizz Buzz"
+	case n%3 == 0:
+		return "Fizz"
+	case n%5 == 0:
+		return "Buzz"
+	}
+	return strconv.Itoa(n)
 }
