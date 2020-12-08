6f15844c40dd692c5969bd92ad15956ef776a756
Mike Donnici <michael.donnici@dpie.nsw.gov.au>
2020-12-09 08:41:55 +1100

    TS course

--- typescript/course/app.ts
+++ typescript/course/app.ts
@@ -1,16 +1,6 @@
-// Union Types allows for multiple types to satisfy a requirement, use the | operator.
-
-// this function only works with numbers
-function add(n1: number, n2: number | string) {
-    return n1 + n2
+const person = {
+    name: "Mike",
+    age: 50,
 }
 
-// this version works with numbers and strings
-// function combined(item1: number | string, )
-
-console.log(add(1,'2'))
-
-let user: string
-user = "Mike"
-user = false
-
+console.log(typeof person)
