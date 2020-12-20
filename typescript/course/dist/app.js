2194eeb73ac22fc485cb30106b63d880eddaca1b
Mike Donnici <michael.donnici@dpie.nsw.gov.au>
2020-12-21 08:53:31 +1100

    TS classes

--- typescript/course/dist/app.js
+++ typescript/course/dist/app.js
@@ -1,5 +1,16 @@
 "use strict";
-function error(msg, code) {
-    throw { message: msg, code: code };
+class Foo {
+    constructor(a = 1) {
+        this.a = a;
+    }
+    get aVal() {
+        return this.a;
+    }
+    set aVal(n) {
+        this.a = n;
+    }
 }
-console.log(error('Something went wrong', 500));
+const f = new Foo(6);
+console.log(f.aVal);
+f.aVal = 10;
+console.log(f.aVal);
--- typescript/course/src/app.ts
+++ typescript/course/src/app.ts
@@ -1,5 +1,17 @@
-function error(msg: string, code: number) {
-    throw {message: msg,  code: code}
+class Foo {
+
+    constructor(private a = 1) {}
+
+    get aVal() {
+        return this.a
+    }
+
+    set aVal(n: number) {
+        this.a = n
+    }
 }
 
-console.log(error('Something went wrong', 500))
+const f = new Foo(6)
+console.log(f.aVal)
+f.aVal = 10
+console.log(f.aVal)
